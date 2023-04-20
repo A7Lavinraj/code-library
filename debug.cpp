81289241d15b34a22cfc920401934f55efcae06f
A7Lavinraj <lavinrajmohan@gmail.com>
2023-04-20 22:17:17 +0530

    debug.cpp was improved

--- debug.cpp
+++ debug.cpp
@@ -1,26 +1,30 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void DBG(int x) {cerr << x;}
-void DBG(long x) {cerr << x;}
-void DBG(long long x) {cerr << x;}
-void DBG(unsigned x) {cerr << x;}
-void DBG(unsigned long x) {cerr << x;}
-void DBG(unsigned long long x) {cerr << x;}
-void DBG(float x) {cerr << x;}
-void DBG(double x) {cerr << x;}
-void DBG(long double x) {cerr << x;}
-void DBG(char x) {cerr << '\'' << x << '\'';}
-void DBG(const char *x) {cerr << '\"' << x << '\"';}
-void DBG(const string &x) {cerr << '\"' << x << '\"';}
-void DBG(bool x) {cerr << (x ? "true" : "false");}
- 
-template<typename T, typename V>void DBG(const pair<T, V> &x);
-template<typename T>void DBG(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), DBG(i); cerr << "}";}
-template<typename T, typename V>void DBG(const pair<T, V> &x) {cerr << '{'; DBG(x.first); cerr << ", "; DBG(x.second); cerr << '}';}
-void dbg() {cerr << "]\n";}template <typename T, typename... V>void dbg(T t, V... v) {DBG(t); if (sizeof...(v)) cerr << ", "; dbg(v...);}
+void debug(int x) {cerr << x;}
+void debug(long x) {cerr << x;}
+void debug(long long x) {cerr << x;}
+void debug(unsigned x) {cerr << x;}
+void debug(unsigned long x) {cerr << x;}
+void debug(unsigned long long x) {cerr << x;}
+void debug(float x) {cerr << x;}
+void debug(double x) {cerr << x;}
+void debug(long double x) {cerr << x;}
+void debug(char x) {cerr << '\'' << x << '\'';}
+void debug(const char *x) {cerr << '\"' << x << '\"';}
+void debug(const string &x) {cerr << '\"' << x << '\"';}
+void debug(bool x) {cerr << (x ? "true" : "false");}
+   
+template<typename T, typename V>void debug(const pair<T, V> &x);
+template<typename T>void debug(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), debug(i); cerr << "}";}
+template<typename T, typename V>void debug(const pair<T, V> &x) {cerr << '{'; debug(x.first); cerr << ", "; debug(x.second); cerr << '}';}
+template<typename ST> void debug(stack<ST> st) { cerr << " "; while(!st.empty()) { cerr << st.top() << " "; st.pop(); }}
+template<typename Q> void debug(queue<Q> que) { while(!que.empty()) { cerr << que.front() << " "; que.pop(); }}
+template<typename DQ> void debug(deque<DQ> dque) { while(!dque.empty()) { cerr << dque.front() << " "; dque.pop_front(); }}
+void dbg() {cerr << "]\n";}template <typename T, typename... V>void dbg(T t, V... v) {debug(t); if (sizeof...(v)) cerr << ", "; dbg(v...);}
+
 #ifdef RECB
-#define dbg(x...) cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = ["; dbg(x); cerr << "\e[39m" << endl;
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; dbg(x); cerr << "\e[39m" << endl;
 #else
 #define dbg(x...) 32
 #endif
